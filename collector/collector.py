8ecd0e433701e7ebed80adc74a3e4f1e7c1dc61a
Trevor Gray <grayt02@moravian.edu>
2023-11-17 10:41:58 -0500

    committing

--- collector/collector.py
+++ collector/collector.py
@@ -8,7 +8,12 @@ r = redis.Redis(host=host, port=port, decode_responses=True)
 
 if __name__ == '__main__':
 
-    r.ltrim('items', 1, 0)
-    r.rpush('items', json.dumps({'name': 'tfrrs1', 'value': 'res1'}))
-    r.rpush('items', json.dumps({'name': 'tfrrs2', 'value': 'res2'}))
-    r.rpush('items', json.dumps({'name': 'tfrrs3', 'value': 'res3'}))
+    r.ltrim('items1', 1, 0)
+    r.rpush('items1', json.dumps({'name': 'tfrrs1', 'value': 'res1'}))
+    r.rpush('items1', json.dumps({'name': 'tfrrs2', 'value': 'res2'}))
+    r.rpush('items1', json.dumps({'name': 'tfrrs3', 'value': 'res3'}))
+
+    r.ltrim('items2', 1, 0)
+    r.rpush('items2', json.dumps({'name': 'tfrrs1', 'value': 'res1'}))
+    r.rpush('items2', json.dumps({'name': 'tfrrs2', 'value': 'res2'}))
+    r.rpush('items2', json.dumps({'name': 'tfrrs3', 'value': 'res3'}))
