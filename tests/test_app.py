e8ea5926458e3ae8cb634a2727a9994201d65e35
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2023-12-02 22:36:13 -0500

    added some unit tests

---
+++ tests/test_app.py
@@ -0,0 +1,43 @@
+import pytest
+import redis
+import json
+
+from server.server import app
+
+def test_index():
+
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_seasons():
+
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    response = client.get('/seasons')
+    assert response.status_code == 200
+
+def test_currentSeason():
+
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    response = client.get('/currentSeason')
+    assert response.status_code == 200
+
+def test_results_4249m():
+
+    app.config['TESTING'] = True
+    client = app.test_client(1)
+
+    response = client.get('/results?season=4249&gender=m')
+    
+    # read text from expected_results.json
+    # compare to response.data
+
+    expected = open('expected_results.json', 'r').read()
+    assert json.loads(response.data.decode(encoding='utf-8')) == json.loads(expected)
+    assert response.status_code == 200
